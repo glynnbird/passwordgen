4c0a9117dd174435440f625083c705f5536778f5
Glynn Bird <glynn.bird@gmail.com>
2025-06-26 14:02:22 +0100

    upgrade to terraform provider v5

--- terraform/pages.tf
+++ terraform/pages.tf
@@ -4,26 +4,34 @@ resource "cloudflare_pages_project" "frontend_project" {
   name              = "pass"
   production_branch = "main"
   
-  build_config {
+  build_config = {
     destination_dir     = "dist"
     root_dir            = "/"
   }
 
-  source {
+  source = {
     type = "github"
-    config {
+    config = {
       owner                         = "glynnbird"
       repo_name                     = "passwordgen"
       production_branch             = "main"
     }
   }
-    deployment_configs {
-      preview {
-        
+    deployment_configs = {
+      preview = {
+                env_vars = {
+          NODE_VERSION = {
+            type = "plain_text"
+            value = "22"
+          }
+        }
       }
-      production {
-        environment_variables = {
-          NODE_VERSION = "20"
+      production = {
+        env_vars = {
+          NODE_VERSION = {
+            type = "plain_text"
+            value = "22"
+          }
         }
       }
   }
@@ -32,10 +40,10 @@ resource "cloudflare_pages_project" "frontend_project" {
 resource "cloudflare_pages_domain" "frontend_domain" {
   account_id   = var.cloudflare_account_id
   project_name = cloudflare_pages_project.frontend_project.name
-  domain       = var.cloudflare_domain
+  name       = var.cloudflare_domain
 }
 
-resource "cloudflare_record" "frontend_dns" {
+resource "cloudflare_dns_record" "frontend_dns" {
   zone_id = var.cloudflare_zone_id
   name    = "pass"
   content   = cloudflare_pages_project.frontend_project.subdomain
